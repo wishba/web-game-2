f2b885cb3623c1027b2761cc5f8bfc451416108d
wishba <wisnubayu34@gmail.com>
2023-09-20 19:55:47 +0700

    adding text-shadow to the debut

--- src/components/ObjectSelector.jsx
+++ src/components/ObjectSelector.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './ObjectSelector.css'
 import data from '../data/data.json'
 import TileSelector from './TileSelector'
 
-function ObjectSelector({ asset, tile, placement }) {
+function ObjectSelector({ asset, heroStep, select }) {
   const objectArray = []
+  const [placement, setPlacement] = useState(select.placement[1])
+
+  useEffect(() => {
+    if (select.position !== undefined) {
+      for (const iterator of select.position[0]) {
+        if (iterator[0] === heroStep[0] && iterator[1] === heroStep[1]) {
+          setPlacement(select.placement[0])
+        }
+      }
+
+      for (const iterator of select.position[1]) {
+        if (iterator[0] === heroStep[0] && iterator[1] === heroStep[1]) {
+          setPlacement(select.placement[1])
+        }
+      }
+    } else {
+      setPlacement(select.placement)
+    }
+  }, [heroStep])
+
 
   for (let index = 0; index < placement.length; index++) {
     objectArray.push(<div key={index} className='ObjectSelector' style={{
@@ -13,7 +33,7 @@ function ObjectSelector({ asset, tile, placement }) {
       top: `calc(var(--tile-size) * -1 * ${placement[index][1]})`,
       zIndex: `${placement[index][2]}`,
     }}>
-      <TileSelector asset={asset} tile={[tile[index][0], tile[index][1]]} />
+      <TileSelector asset={asset} tile={[select.tile[index][0], select.tile[index][1]]} />
     </div>
     )
   }
